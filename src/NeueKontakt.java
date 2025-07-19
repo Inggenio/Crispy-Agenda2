66dd73d26ea6fc8e426aebd02a9b43ef3c6f6e68
inggenio <inggenio13@gmail.com>
2025-07-20 00:41:57 +0200

    20/07 Kontakt 1° Funktionsfähig Programm

--- src/NeueKontakt.java
+++ src/NeueKontakt.java
@@ -1,16 +1,39 @@
+/*
+ MIT License
+
+ Copyright (c) 2025 [Tu Nombre o Usuario GitHub]
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+*/
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class NeuKontakt {
+public class NeueKontakt {
 
 	private KontaktTableModel tableModel; // Referenz an Original Instanz der Tabelle
 
-	public NeuKontakt(KontaktTableModel model) {
+	public NeueKontakt(KontaktTableModel model) {
 		this.tableModel = model;
 	}
 
