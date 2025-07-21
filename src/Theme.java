0217f089a57de9605c9161fc886e167b977a1f6d
inggenio <inggenio13@gmail.com>
2025-07-21 22:50:30 +0200

    21/07 Erweiterung mit Adresse, PLZ und Stadt

--- src/Theme.java
+++ src/Theme.java
@@ -1,2 +1,25 @@
+import java.awt.*;
+
 public class Theme {
+	public static Theme DARK = new Theme(
+			Color.DARK_GRAY,     // Background
+			Color.LIGHT_GRAY,    // Foreground
+			Color.ORANGE         // Buchstaben
+	);
+
+	public static Theme LIGHT = new Theme(
+			Color.WHITE,
+			Color.BLACK,
+			Color.BLUE
+	);
+
+	public Color background;
+	public Color foreground;
+	public Color accent;
+
+	public Theme(Color bg, Color fg, Color acc) {
+		this.background = bg;
+		this.foreground = fg;
+		this.accent = acc;
+	}
 }
